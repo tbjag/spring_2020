fb7d437ee294b4c7a43d964ec87c8adfd92d8356
tbjag <tbjagodits@gmail.com>
2020-01-21 21:49:09 -0500

    added pledge and comment

--- cs370/largesum/largesum.cpp
+++ cs370/largesum/largesum.cpp
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Name        : largesum.cpp
+ * Authors     : Theodore Jagodits, Alex Kubecka, Kurt Von Autenried
+ * Version     : 1.0 
+ * Date        : 1/21/2020
+ * Description : add large 50 digit numbers together
+ *  https://projecteuler.net/problem=13
+ * "I pledge my honor that I have abided by the Stevens Honor System"
+ ******************************************************************************/
+
 #include <iostream>
 
 using namespace std;
