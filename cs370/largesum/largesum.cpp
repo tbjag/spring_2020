45d2700a88d4a4ee52e38da58a3355b08bd7959b
tbjag <tbjagodits@gmail.com>
2020-01-23 22:03:10 -0500

    works

--- cs370/largesum/largesum.cpp
+++ cs370/largesum/largesum.cpp
@@ -6,25 +6,36 @@
  * Description : add large 50 digit numbers together
  *  https://projecteuler.net/problem=13
  * "I pledge my honor that I have abided by the Stevens Honor System"
+ * reference: https://www.geeksforgeeks.org/sum-two-large-numbers/
  ******************************************************************************/
 
 #include <fstream>
 #include <iostream>
+#include <bits/stdc++.h>
+#include <string>//maybe look for \0 instead of this
 
 using namespace std;
 
+//reverse string function??
+
+
 int main(){
+    string sum = "";
     string line;
     ifstream myfile ("test.txt");
     if (myfile.is_open())
     {
-        while ( getline (myfile,line) )
-        {
-        cout << line << '\n';
+        while ( getline (myfile,line) ){
+        //reverse the line
+        reverse(line);
+        //in every line
+        for(int i = 0; i < line.length(); i++){
+            //string t = line[i] + 
+            }
         }
         myfile.close();
     } else{
-        cout << "Unable to open the final";
+        cout << "Unable to open the file";
     }
     return 0;
 }
