b699f18469a2089a3be56fc019ca32610008a478
Theodore J <tbjagodits@gmail.com>
2020-02-06 21:27:15 -0500

    python finis

---
+++ ma331/hw2.py
@@ -0,0 +1,30 @@
+from statistics import *
+from datetime import datetime
+import csv
+import math
+
+#for hw2 because its stupid and i need data
+
+data = {}
+
+n = 20
+s_mean = 2
+std = 9
+
+def getSample(i):
+    sT = datetime.now()
+    return NormalDist(s_mean, std).samples(n, seed = i)
+
+
+
+with open('hw2.csv', mode = '+w') as csv_file:
+    fieldnames = ['equation1', 'equation2']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for i in range(100):
+        y = getSample(i)
+        eq1 = (mean(y) - 2)/(math.sqrt(std/n))
+        eq2 = ((n-1)*stdev(y)) / std
+        writer.writerow({'equation1': eq1, 'equation2' : eq2})
+
