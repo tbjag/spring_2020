f4cb3470582244b5869ee846e109184c3097ea78
Theodore J <tbjagodits@gmail.com>
2020-03-05 12:29:07 -0500

    added hw4 ma

--- cs677/hw3_sobel_filter/sobel.cu
+++ cs677/hw3_sobel_filter/sobel.cu
@@ -134,27 +134,27 @@ __global__ void sobel_filter(int *inputM, int *outputM, int width, int height, i
 	// shared 16x16
 	__shared__ int local[TILE_SIZE][TILE_SIZE];
 	
-	//set up vars, leave 1 px border
+	//set up vars
 	int tx = threadIdx.x; int ty = threadIdx.y;
-	//find positions within input array for 14x14
-	int row = blockIdx.x * 14 + tx +1; 
-	int col = blockIdx.y * 14 + ty +1;
+	//find positions within input array for 14x14 offset by one for pixel border
+	int row = blockIdx.x * 14 + tx + 1; 
+	int col = blockIdx.y * 14 + ty + 1;
 	
 	//correct to the right place in input
 	int place = (row-1)*width + col-1;
 
-	//if in bounds of pic -- check?
-	if((row < width) && (col < height)){
+	//if in bounds of pic
+	if((row - 1 < width) && (col - 1 < height)){
 		//store into shared
 		local[tx][ty] = inputM[place];
 		//find inner part of 14x14
-		if((tx > 0) && (tx < 15) && (ty > 0) && (ty < 15) && (row + 1 < width) && (col + 1  < height)){
+		if((tx > 0) && (tx < 15) && (ty > 0) && (ty < 15) && (row + 1 < width) && (col + 1 < height)){// <-- check reasoning
 			//reuse vars
 			int top_left = local[tx-1][ty-1], top_right = local[tx+1][ty-1];
 			int bot_left = local[tx-1][ty+1], bot_right = local[tx+1][ty+1];
 			int gx = top_left - top_right + 2*local[tx-1][ty] - 2*local[tx+1][ty] + bot_left - bot_right;
 			int gy = top_left + 2*local[tx][ty-1] + top_right - bot_left - 2*local[tx][ty+1] - bot_right;
-			//int gx = 0, gy =0;
+
 			// calculate magnitude
 			int magnitude = gx*gx + gy*gy;
 			int result = 0;
